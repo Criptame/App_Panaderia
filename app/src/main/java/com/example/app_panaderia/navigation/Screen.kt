e5b462c888e888b7def933767b079263e1d5eb82
Guillermo <guillermosantander343@gmail.com>
2026-01-21 05:02:45 -0300

    Quinto Commit

---
+++ app/src/main/java/com/example/app_panaderia/navigation/Screen.kt
@@ -0,0 +1,19 @@
+package com.example.app_panaderia.navigation
+
+sealed class Screen(val route:String){
+    data object Admin:Screen (route = "admin_page")
+    data object Vizu:Screen(route = "vizu_page")
+    data object ConfigPed:Screen(route = "configped_page")
+    data object AñadirCom:Screen(route = "añadircom_page")
+    data object AñadirPed:Screen(route = "añadirped_page")
+    data object Com:Screen(route = "com_page")
+    data object DeteleCom:Screen(route = "deletecom_page")
+    data object Ped:Screen(route = "ped_page")
+
+    data class Detail (val itemId:String):Screen(route = "detail_page/{itemId}"){
+
+        fun buidRoute():String {
+            return route.replace("{itemId}",itemId)
+        }
+    }
+}
