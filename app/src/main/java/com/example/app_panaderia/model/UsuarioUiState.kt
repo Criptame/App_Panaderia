6edcf6347a3f1ad03a93fea3451483f453ef4015
fergonzalez1717 <guillermosantander343@gmail.com>
2026-01-21 21:30:34 -0300

    Cambios a model

---
+++ app/src/main/java/com/example/app_panaderia/model/UsuarioUiState.kt
@@ -0,0 +1,8 @@
+package com.example.app_panaderia.model
+
+data class UsuarioUiState(
+    val id: String = "",
+    val nombre: String = "",
+    val email: String = "",
+    val esAdmin: Boolean = false
+)
