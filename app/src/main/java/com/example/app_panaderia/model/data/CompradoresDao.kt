37cb32d3330f4bbd8ffc7ca47547c3ccd921de3d
BruceXV <guillermosantander343@gmail.com>
2026-01-26 19:33:47 -0300

    Reset

---
+++ app/src/main/java/com/example/app_panaderia/model/data/CompradoresDao.kt
@@ -0,0 +1,22 @@
+package com.example.app_panaderia.model.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CompradoresDao {
+    @Query("SELECT * FROM compradores ORDER BY id DESC")
+    fun getAll(): Flow<List<Comprador>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(comprador: Comprador): Long
+
+    @Update
+    suspend fun update(comprador: Comprador)
+
+    @Delete
+    suspend fun delete(comprador: Comprador)
+
+    @Query("SELECT * FROM compradores WHERE id = :id LIMIT 1")
+    suspend fun findById(id: Long): Comprador?
+}
