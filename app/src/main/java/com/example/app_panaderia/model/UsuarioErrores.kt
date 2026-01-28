80c810d2a570eacb2563521a7f59a78f617bee80
BruceXV <guillermosantander343@gmail.com>
2026-01-28 19:00:26 -0300

    AD

--- app/src/main/java/com/example/app_panaderia/model/UsuarioErrores.kt
+++
@@ -1,7 +0,0 @@
-package com.example.app_panaderia.model
-
-data class UsuarioErrores(
-    val nombreError: String = "",
-    val emailError: String = "",
-    val passwordError: String = ""
-)
--- app/src/main/java/com/example/app_panaderia/model/UsuarioUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.app_panaderia.model
-
-data class UsuarioUiState(
-    val id: String = "",
-    val nombre: String = "",
-    val email: String = "",
-    val esAdmin: Boolean = false
-)
